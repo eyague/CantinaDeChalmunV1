8a3a46407f0461bc77f36a29d173f73db0bcd034
Alvaro Oteros <aoteros@infobosccoma.net>
2016-04-05 19:37:26 +0200

    Creacio MenuActivity Java

---
+++ app/src/main/java/net/infobosccoma/cantinadechalmunv1/MenuActivity.java
@@ -0,0 +1,18 @@
+package net.infobosccoma.cantinadechalmunv1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by aoteros on 05/04/2016.
+ */
+public class MenuActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+
+}
