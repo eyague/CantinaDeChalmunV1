73bd4188f48ab690fbe2e240fc2fea36fe675412
Alvaro Oteros <aoteros@infobosccoma.net>
2016-04-05 20:45:40 +0200

    Creacio MenuActivity Java

--- app/src/main/java/net/infobosccoma/cantinadechalmunv1/MenuActivity.java
+++ app/src/main/java/net/infobosccoma/cantinadechalmunv1/MenuActivity.java
@@ -1,18 +1,64 @@
 package net.infobosccoma.cantinadechalmunv1;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.IOException;
 
 /**
  * Created by aoteros on 05/04/2016.
  */
-public class MenuActivity extends AppCompatActivity{
+public class MenuActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
+
+    private MediaPlayer mediaPlayer;
+    static final String AUDIO_PATH = "http://www.susanpiver.com/audio/Dancing%20in%20the%20Dark.mp3";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnPreparedListener(this);
+        //mediaPlayer.isPlaying(); per si mentres esta sonant per lo del looping
+        playAudio(); //Implementar el subprograma perque s'executi la musica.
+
+        try {
+            mediaPlayer.setDataSource(AUDIO_PATH);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
+    public void onClickVideo(View v) {
+        setContentView(R.layout.activity_video);
+    }
 
+    public void onClickContingut(View v) {
+        setContentView(R.layout.activity_contingut);
+    }
+
+    public void onClickAudio(View v) {
+        setContentView(R.layout.activity_audio);
+    }
+
+    private void playAudio() {
+        mediaPlayer.prepareAsync();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+        mp.setLooping(true);//serveix per fer que la musica faci looping.
+    }
+
+    @Override
+    protected void onDestroy() {
+        mediaPlayer.release();
+        super.onDestroy();
+    }
 }
