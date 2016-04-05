3678ee7d4e59073d0a9a37937567a4e027817f30
Darkyague <eyague@infobosccoma.net>
2016-04-05 20:47:39 +0200

    commit edit mainactivity

--- app/src/main/java/net/infobosccoma/cantinadechalmunv1/MainActivity.java
+++ app/src/main/java/net/infobosccoma/cantinadechalmunv1/MainActivity.java
@@ -2,6 +2,10 @@ package net.infobosccoma.cantinadechalmunv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //eric
+    }
+
+    public void onClickFoto(View v) {
+        //fer foto sense guardar.
+
+    }
+
+    public void onClickLoggin(View v) {
+        // entrara a l'aplicació miran si te els nom i cognom entrats, i si s'ha fet la foto
+
+        TextView nom = (TextView) findViewById(R.id.editTextNom);
+        TextView cognom = (TextView) findViewById(R.id.editTextCognom);
+
+        if (!(nom.getText().toString().equals("") && cognom.getText().toString().equals(""))) {
+            //entrar a l'aplicació
+            setContentView(R.layout.activity_menu);
+
+        } else {
+            //torrada per dir que falta alguna cosa.
+
+        }
+
     }
 }
